8c16a1f07d7e6931a275a09b632de9087ed8b2c7
rizwan <rizwan.sha993@gmail.com>
2023-07-22 20:49:51 +0530

    arrays: easy 11 problems

--- README.md
+++ README.md
@@ -187,3 +187,103 @@ TC is `O(n)` because the recursion tree is linear. In case of fibonacci almost e
 <img alt="summaryAlgoTimeComplexity.png" src="src/main/resources/static/sortingAlgoTimeComplexity.png" width="500"/>
 
 References: Thanks to Shahad (https://blog.shahadmahmud.com/) for the GIFs of sorting algorithms.
+
+## Arrays:
+### Easy Problems
+#### 1. [Largest Element in Array](https://practice.geeksforgeeks.org/problems/largest-element-in-array4009/0?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=largest-element-in-array)
+* Approach: 
+  * Traverse the array and keep track of the largest element.
+* TC = O(N) & SC = O(1)
+
+#### 2. [Second Largest](https://practice.geeksforgeeks.org/problems/second-largest3735/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=second-largest)
+Approach:
+* Bruteforce: 
+  * Sort the array and return the first from the end (second largest) that is not equal to the last element (largest)
+  * TC = O(Nlog(N)) & SC = O(1)
+* Optimal:
+  * Keep track of largest 
+  * Traverse the array and change largest and secondLargest based on the element you encounter.
+  * TC = O(N) & SC = O(1)
+
+#### 3. [Check if array is sorted](https://practice.geeksforgeeks.org/problems/check-if-an-array-is-sorted0701/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=check-if-an-array-is-sorted)
+* Approach:
+  * Traverse the array and compare the element with the next element and then decide
+* TC = O(N) & SC = (1)
+
+#### 4. [Remove duplicate elements from sorted Array](https://practice.geeksforgeeks.org/problems/remove-duplicate-elements-from-sorted-array/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=remove-duplicate-elements-from-sorted-array)
+Approach:
+* Bruteforce:
+  * Unique elements? Use hashset. Also want to preserve the insertion order? Use LinkedHashSet.
+  * TC = O(N) & SC = O(N)
+* Optimal:
+  * Use 2 pointers, one is used to traverse the array and other is incremented and assigned with the first one.
+  * Keep increment count whenever you get distinct elements.
+  * TC = O(N) & SC = O(1)
+
+#### 5. [Left Rotate an Array by One](https://www.codingninjas.com/studio/problems/left-rotate-an-array-by-one_5026278)
+* Approach:
+  * Store the first element and keep shifting each element by one left.
+  * Place the stored element at the last.
+* TC = O(N) & SC = O(1)
+
+#### 6. [Left Rotate an Array by D Places](https://practice.geeksforgeeks.org/problems/reversal-algorithm5340/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=reversal-algorithm)
+Approach:
+* Bruteforce:
+  * Repeat the "Left Rotate an array by one" for D times
+  * TC = O(N^2) & SC =  O(1)
+* Better:
+  * Create a duplicate array
+  * Copy the contents from D to N-1 to the duplicate array, then
+  * Copy the contents from 0 to D-1 to the duplicate array
+  * TC = O(N) & SC = O(N)
+* Optimal:
+  * This is a trick that can be observed
+  * Reverse the sub-array from 0 to D-1 
+  * Reverse the sub-array from D to N-1
+  * Reverse the complete array
+  * TC = O(N) & SC = O(1)
+
+#### 7. [Move all zeroes to end of array](https://practice.geeksforgeeks.org/problems/move-all-zeroes-to-end-of-array0751/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=move-all-zeroes-to-end-of-array)
+Approach:
+* Bruteforce:
+  * Create a duplicate array
+  * Traverse the original array and push all non zero elements on to the duplicate array.
+  * Fill the remaining elements with zeroes
+  * TC = O(N) & SC = O(N)
+* Optimal:
+  * Use 2 Pointers, One will keep track of zeroes, Other will keep track of non-zeroes
+  * Swap the contents of both if the first has non-zero and second has zero
+  * TC = O(N) & SC = O(1)
+
+#### 8. [Searching an element in a sorted array](https://practice.geeksforgeeks.org/problems/who-will-win-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=who-will-win)
+* Approach:
+  * Simple linear search
+* TC = O(N) & SC = O(1)
+
+#### 9. [Union of Two Sorted Arrays](https://practice.geeksforgeeks.org/problems/union-of-two-sorted-arrays-1587115621/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=union-of-two-sorted-arrays)
+* Approach:
+  * Use 2 pointers, one for the first array and other for the second
+  * Compare the contents of both the array, Add the one which is lower
+  * Remember to not add the element which is already present
+* TC = O(n+m) & SC = O(n+m)
+
+#### 10. [Missing number](https://practice.geeksforgeeks.org/problems/missing-number4257/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=missing-number)
+Approach:
+* Bruteforce
+  * Traverse the array and check if all natural numbers are present.
+  * If a number is absent return it
+  * TC = O(N^2) & SC = O(1)
+* Better
+  * Use a HashMap to count the occurrence of the numbers
+  * A key that has 0 value is our answer
+  * TC = O(N) & SC = O(N)
+* Optimal
+  * Find the summation of N natural numbers
+  * Find the summation of the entire array
+  * Subtract the second from first
+  * TC = O(N) & SC = O(1)
+
+#### 11. [Max Consecutive Ones](https://leetcode.com/problems/max-consecutive-ones/)
+* Approach:
+  * Basic Implementation: Keep track of consecutive ones and return the max
+* TC = O(N) & SC = O(1)
---
+++ src/main/java/arrays/easy/EasyArrayProblems.java
@@ -0,0 +1,269 @@
+package arrays.easy;
+
+import java.util.*;
+
+public class EasyArrayProblems {
+
+    // Max Consecutive Ones
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        for (int num : nums) {
+            if (num == 1) count++;
+            else {
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        max = Math.max(max, count);
+        return max;
+    }
+
+    // Missing Number - Optimal
+    public static int missingNumber3(int A[], int N) {
+        // Functional code to find the sum of an array. We can also use for-loop
+        int sum = Arrays.stream(A).sum();
+
+        int expectedSum = (N * (N+1)) / 2;
+        return expectedSum - sum;
+    }
+
+    // Missing Number - Better
+    public static int missingNumber2(int A[], int N) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            hashMap.put(i, 0);
+        }
+        for (int num : A) {
+            hashMap.put(num, 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : hashMap.entrySet()) {
+            if (entry.getValue() == 0) return entry.getKey();
+        }
+        return -1;
+    }
+
+    // Missing Number - Bruteforce
+    public static int missingNumber(int A[], int N) {
+        for (int i = 1; i <= N; i++) {
+            boolean numFound = false;
+            for (int j = 0; j < N; j++) {
+                if (A[j] == i) {
+                    numFound = true;
+                    break;
+                }
+            }
+            if (!numFound) return i;
+        }
+        return -1;
+    }
+
+
+    // Union of the two sorted arrays.
+    public static ArrayList<Integer> findUnion(int arr1[], int arr2[], int n, int m) {
+        ArrayList<Integer> list = new ArrayList<>();
+        int i = 0;
+        int j = 0;
+
+        // inserting the first element so that we don't have to check for empty list
+        // while getting the peak element
+        list.add(Math.min(arr1[i], arr2[j]));
+
+        while (i < n && j < m) {
+            if (arr1[i] == list.get(list.size()-1)) i++;
+            else if (arr2[j] == list.get(list.size()-1)) j++;
+            else if (arr1[i] < arr2[j]) list.add(arr1[i++]);
+            else if (arr2[j] < arr1[i]) list.add(arr2[j++]);
+            else {
+                list.add(arr1[i++]);
+                j++;
+            }
+        }
+
+        while (i < n) {
+            if (arr1[i] != list.get(list.size()-1)) list.add(arr1[i]);
+            i++;
+        }
+
+        while (j < m) {
+            if (arr2[j] != list.get(list.size()-1)) list.add(arr2[j]);
+            j++;
+        }
+
+        return list;
+    }
+
+    // Searching an element in a sorted array
+    static int searchInSorted(int arr[], int N, int K) {
+        int result = -1;
+        for (int i = 0; i < N; i++) {
+            if (arr[i] == K)
+                return 1;
+        }
+        return -1;
+    }
+
+    // Move all zeroes to end of array - Optimal
+    void pushZerosToEnd2(int[] arr, int n) {
+        int j = 0;
+        while (j < n && arr[j] != 0) {
+            j++;
+        }
+
+        for (int i = j+1; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[j] = arr[i];
+                arr[i] = 0;
+                j++;
+            }
+        }
+    }
+
+    // Move all zeroes to end of array - Bruteforce
+    void pushZerosToEnd(int[] arr, int n) {
+        int[] duplicateArr = new int[n];
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                duplicateArr[j++] = arr[i];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (i < j) {
+                arr[i] = duplicateArr[i];
+            } else {
+                arr[i] = 0;
+            }
+        }
+
+    }
+
+    // Left Rotate an Array by D Places - Optimal Clean Code
+    void leftRotate3(int[] arr, int n, int d) {
+        reverseArray(arr, 0, d-1);
+        reverseArray(arr, d, n-1);
+        reverseArray(arr, 0, n-1);
+    }
+
+    private static void reverseArray(int[] arr, int low, int high) {
+        while (low < high) {
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            low++;
+            high--;
+        }
+    }
+
+    // Left Rotate an Array by D Places - Better
+    void leftRotate2(int[] arr, int n, int d) {
+        int duplicateArr[] = new int[n];
+        int j = 0;
+        for (int i = d; i < n; i++) {
+            duplicateArr[j++] = arr[i];
+        }
+        for (int i = 0; i < d; i++) {
+            duplicateArr[j++] = arr[i];
+        }
+        for (int i = 0; i < n; i++) {
+            arr[i] = duplicateArr[i];
+        }
+    }
+
+    // Left Rotate an Array by D Places - Bruteforce
+    void leftRotate(int[] arr, int n, int d) {
+        while (d > 0) {
+            int temp = arr[0];
+            for (int i = 1; i < n; i++) {
+                arr[i - 1] = arr[i];
+            }
+            arr[n - 1] = temp;
+            d--;
+        }
+    }
+
+    // Left Rotate an Array by One
+    public static int[] rotateArray(int[] arr, int n) {
+        int temp = arr[0];
+        for (int i = 1; i < n; i++) {
+            arr[i-1] = arr[i];
+        }
+        arr[n-1] = temp;
+        return arr;
+    }
+
+    // Remove duplicate elements from sorted Array - Optimal
+    public static int remove_duplicate2(int A[],int N){
+        int i = 0;
+        int j = 1;
+        int count = 1;
+
+        while (j < N) {
+            if (A[j] != A[i]) {
+                A[++i] = A[j];
+                count++;
+            }
+            j++;
+        }
+        return count;
+    }
+
+    // Remove duplicate elements from sorted Array - Bruteforce
+    public static int remove_duplicate(int A[],int N){
+        LinkedHashSet<Integer> hashSet = new LinkedHashSet<>();
+        for (int num : A) {
+            hashSet.add(num);
+        }
+
+        int i = 0;
+        for (int num : hashSet) {
+            A[i++] = num;
+        }
+        return hashSet.size();
+    }
+
+    // Check if array is sorted
+    public static boolean arraySortedOrNot(int[] arr, int n) {
+        for (int i = 0; i < n-1; i++) {
+            if (arr[i+1] < arr[i]) return false;
+        }
+        return true;
+    }
+
+    // Second largest - optimal
+    public static int print2largest2(int arr[], int n) {
+        int max = arr[0];
+        int secondMax = -1;
+
+        for (int i = 1 ; i < n; i++) {
+            if (arr[i] > max) {
+                secondMax = max;
+                max = arr[i];
+            } else if (arr[i] > secondMax && arr[i] < max) {
+                secondMax = arr[i];
+            }
+        }
+        return secondMax;
+    }
+
+    // Second largest - bruteforce
+    public static int print2largest(int arr[], int n) {
+        Arrays.sort(arr);
+        int max = arr[n-1];
+        for (int i = n-2; i >= 0; i--) {
+            if (arr[i] != max) return arr[i];
+        }
+        return -1;
+    }
+
+
+    // Largest Element in Array
+    public static int largest(int arr[], int n) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+        }
+        return max;
+    }
+}
